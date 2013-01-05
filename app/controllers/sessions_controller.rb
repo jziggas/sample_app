ea78bb0dd17948a11be70a9117522d20fcfc09f2
Joshua Ziggas <zoshjiggas@gmail.com>
2013-01-05 17:31:48 -0500

    ch8 exercises including rspec custom matcher and form_tag

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,29 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      # Sign the user in and redirect to the user's show page
+      sign_in user
+      redirect_to user
+    else
+      # First usage of .now to prevent double flash from appearing ( because a render is not a request)
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+=begin
+    # Refactoring exercise to use form_tag instead of form_for
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Sign the user in and redirect to the user's show page
       sign_in user
       redirect_to user
     else
-      # First usage of .now to prevent double flash from appearing (a render is not a request)
+      # First usage of .now to prevent double flash from appearing ( because a render is not a request)
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
+=end
   end
 
   def destroy
